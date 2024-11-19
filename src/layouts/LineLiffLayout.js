d145f050af23d5b3b2880229ef801c3f42c5180b
sitthiphong.p <sitthiphong.p@pdkm.tech>
2024-11-19 20:04:05 +0700

    update

--- src/layouts/LineLiffLayout.js
+++ src/layouts/LineLiffLayout.js
@@ -6,7 +6,7 @@ const LineLiffLayout = ({ children }) => {
     await liff.init({ liffId: "2006454877-A8GOwb1j" });
     if (liff.isLoggedIn()) {
       const profile = await liff.getProfile();
-      console.log(profile);
+      // console.log(profile);
       
     } else {
       liff.login();
