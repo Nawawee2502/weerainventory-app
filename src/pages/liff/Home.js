f7407657c54c1ac354e609a137250e2c7dda8caa
sitthiphong.p <sitthiphong.p@pdkm.tech>
2024-11-19 19:45:56 +0700

    add: LINE LIFF

---
+++ src/pages/liff/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutsLiff from '../../layouts/LineLiffLayout';
+
+const HomePage = () => {
+  return (
+    <LayoutsLiff>
+      <h1>Welcome to LINE LIFF App</h1>
+    </LayoutsLiff>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/liff/Warehouse.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutsLiff from '../../layouts/LineLiffLayout';
+
+const WarehousePage = () => {
+  return (
+    <LayoutsLiff>
+        <h1>Welcome to LINE LOGIN</h1>
+    </LayoutsLiff>
+  );
+};
+
+export default WarehousePage;
